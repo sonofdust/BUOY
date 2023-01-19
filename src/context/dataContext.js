c7bdf02a2a18c14a6274b436474068483e716c00
sonofdust <sonofdust@gmail.com>
2023-01-19 12:56:35 -0700

    Cnahge

--- src/context/dataContext.js
+++ src/context/dataContext.js
@@ -1,8 +1,8 @@
 import {createContext, useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
-import {format} from "date-fns";
-import api from "../api/posts";
 import useAxiosFetch from "../hooks/useAxiosFetch";
+import api from "../api/posts";
+import format from "date-fns/format";
 
 const DataContext = createContext({});
 
@@ -10,10 +10,6 @@ export const DataProvider = ({children}) => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-  const [editTitle, setEditTitle] = useState("");
-  const [editBody, setEditBody] = useState("");
 
   const navigate = useNavigate();
   const {data, fetchError, isLoading} = useAxiosFetch(
@@ -43,47 +39,6 @@ export const DataProvider = ({children}) => {
     }
   };
 
-  const handleEdit = async (id) => {
-    try {
-      const datetime = format(new Date(), "MMMM dd, yyyy pp");
-      const updatedPost = {
-        id,
-        title: editTitle,
-        datetime,
-        body: editBody,
-      };
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(posts.map((e) => (e.id === id ? {...response.data} : e)));
-      setEditTitle("");
-      setEditBody("");
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const id = posts.length ? Math.max(...posts.map((item) => item.id)) + 1 : 1;
-    const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const newPost = {
-      id,
-      title: postTitle,
-      datetime,
-      body: postBody,
-    };
-    try {
-      const result = await api.post("/posts", newPost);
-      setPosts([...posts, result.data]);
-      setPostBody("");
-      setPostTitle("");
-      navigate("/");
-    } catch (e) {
-      // Not in 200 response range.
-      console.log("Error: ", e.message);
-    }
-  };
-
   return (
     <DataContext.Provider
       value={{
@@ -92,21 +47,11 @@ export const DataProvider = ({children}) => {
         searchResults,
         fetchError,
         isLoading,
-        handleSubmit,
         handleDelete,
-        setPostTitle,
-        postTitle,
-        setPostBody,
-        postBody,
         posts,
-        handleEdit,
-        setEditTitle,
-        editTitle,
-        setEditBody,
-        editBody,
-        format,
-        api,
+        navigate,
         setPosts,
+        format,
       }}
     >
       {children}
